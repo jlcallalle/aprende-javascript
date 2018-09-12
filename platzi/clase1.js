45d6a862aef9fa2fb85c73cf3a82e072cb356077
Jorge <jlcallalle@medialabla.com>
2018-09-12 00:45:06 -0500

    se ordena files - funciones

---
+++ platzi/clase1.js
@@ -0,0 +1,16 @@
+
+//Para declarar una variable se escribe 'var'
+//var no declarada: nombre is not defined, variable declarada sin valor : undefined
+// El ; es opcional, js no obliga, hay casos puntuales
+// podemos declarar variables en la misma linea, juntarlo
+// Javascript es un lenguaje debilmente tipado
+
+var nombre = 'Jorge', apellido = 'Callalle'
+var edad = 28
+
+edad = '28 años'
+
+console.log('Hola ' + nombre + ' ' + apellido);
+console.log('Tengo ' + edad);
+
+var peso = 75;
---
+++ platzi/clase2.js
@@ -0,0 +1,7 @@
+var nombre = 'Jorge', apellido = 'Callalle'
+// invocamos una funcion para la variable nombre: toUpperCase
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = nombre.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadLetrasDelNombre = nombre.lenght;
